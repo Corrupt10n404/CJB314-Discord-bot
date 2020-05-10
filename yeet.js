a78ed524e661e6d41a958dffcad80b4fbac0c42c
Corrupt10n404 <52146696+Corrupt10n404@users.noreply.github.com>
2020-05-10 16:10:05 -0700

    update lul it still don't work
    me spend time updating it but idc it still doesn't work hahah hehhha ahehehae eahguaguiegrkillmeisdfauhguiaberg asvmwef

--- yeet.js
+++ yeet.js
@@ -1,23 +1,31 @@
-function wait(ms){
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-   }
- }
 const Discord = require("discord.js")
-module.exports.run = async (client,message,arguments,config) => {
+module.exports.run = async(client,message,arguments,config) => {
+    if(guild.id == "555087033652215830" || guild.id == 555087033652215830){
+        message.channel.send("do u use discord.py?");
+            const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+            yesresponses = ["yes","ye","yeah","yea","myes","yep"]
+            collector.on('collect', message => {
+                if(yesresponses.indexOf(message.content.toLowerCase) !== -1){
+                    message.channel.send('git band');
+                    collector.stop();
+                }
+            });
+    }
+    if(parseInt(message.author.id) !== 686043677986127899 ){
+        return
+    }
+    if(parseInt(message.author.id) !== 686043677986127899 ){
+        return
+    }
     message.channel.send("Are you sure you want to ban all members in the server?");
     const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
     collector.on('collect', message => {
-        if (message.content === "yes") {
+        if (message.content.toLowerCase() === "yes") {
             message.channel.send("Ok, banning all members").then((message) => {
-                let i = 0
                 let messages = ["Banning all members... 0%","Banning all members... 17%","Banning all members... 36%","Banning all members... 52%","Banning all members... 69%","Banning all members... 81%","Banning all members... 99%"]
-                for (i = 0; i < messages.length; i++){
-                    message.edit(messages[i]); 
-                };
-                collector.stop()
+                var i = 0
+                setInterval(function(){ message.edit(messages[i]); i++ ; if (i >= messages.length){message.channel.send("nah")
+                collector.stop();clearInterval()}}, 3000);
             });
         } else {
             message.channel.send("Canceled")
