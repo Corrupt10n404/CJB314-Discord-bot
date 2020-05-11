8922dc9014724fc372b77ec64795dc34c68fc9cd
Corrupt10n404 <52146696+Corrupt10n404@users.noreply.github.com>
2020-05-10 18:25:32 -0700

    fix
    ok i fix now it work

--- react.js
+++ react.js
@@ -1,21 +1,34 @@
 module.exports.run = async (client,message,arguments,config) => {
+    try{
     var reactmessage
-    if(!arguments){
-        return("syntax: `react [message id]")
+    var reactemoji = arguments[0]
+    if(!arguments||!arguments[0]){
+        return message.channel.send("Syntax: `react <emoji> [message id]")
     }
     if(!arguments[1]){
-        reactmessage = message;
-        arguments = arguments.shift()
+        if(typeof arguments == "array"){
+            reactemoji = arguments[0]
+        }
+        else{
+            reactemoji = arguments
+        }
+        reactmessage = message
     }
     else{
-        reactmessage =  await message.channel.fetchMessage(arguments[0].replace(" ",""));
+        reactmessage = await message.channel.fetchMessage(String(arguments[1]))
     }
-    try{
-        reactmessage.react(arguments[1].replace(" ",""))
+    //message.channel.send(message.channel.fetchMessage(String(arguments[1])).content)
+    reactemoji = reactemoji.replace(" ","");
+    if(reactemoji.indexOf("<") !== -1){
+        reactemoji = reactemoji.split(":")[2].replace(" ","").replace(">","")
     }
-    catch(err){throw err}
+    reactmessage.react(reactemoji);
+    }
+    catch{
+        message.channel.send("Syntax: `react [message id] <emoji>")
+    }
 }
 module.exports.help = {
     name: "react",
-    help: "syntax: `react [message id]"
+    help: "syntax: `react [message id] <emoji>"
 }
